e18cee77d43ac8aa86535416cfd9827d17c2b9a6
rwtema <rwtema@gmail.com>
2014-11-29 19:48:05 +0000

    New properties.

--- src/main/java/thermaldynamics/ducts/item/PropsConduit.java
+++ src/main/java/thermaldynamics/ducts/item/PropsConduit.java
@@ -5,4 +5,7 @@ public class PropsConduit {
     public static final int MAX_ITEMS_TRANSMITTED = 6;
     public static final int FLUID_EMPTY_UPDATE_DELAY = 96;
     public static final byte FLUID_UPDATE_DELAY = 4;
+    public static final byte ENDER_UPDATE_DELAY = 4;
+    public static final int ENDER_TRANSMIT_COST = 500;
+    public static final int STUFF_LIMIT = 10;
 }
