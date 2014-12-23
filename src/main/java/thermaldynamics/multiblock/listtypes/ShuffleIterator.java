fab0dd9be4f29b6668230fe53e398eda788204cc
rwtema <rwtema@gmail.com>
2014-12-23 07:20:24 +0000

    Added a 'shuffle iterator' that will shuffle an arraylist as it is being iterated.

---
+++ src/main/java/thermaldynamics/multiblock/listtypes/ShuffleIterator.java
@@ -0,0 +1,42 @@
+package thermaldynamics.multiblock.listtypes;
+
+import cofh.lib.util.helpers.MathHelper;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ShuffleIterator<T> implements Iterator<T> {
+    ArrayList<T> list;
+    int i = 0;
+    int j = -1;
+    T next;
+
+
+    public ShuffleIterator(ArrayList<T> list) {
+        this.list = list;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (list.size() >= i)
+            return false;
+        next = list.get(i);
+        return true;
+    }
+
+    @Override
+    public T next() {
+        if (i > 0 && i < list.size()) {
+            j = MathHelper.RANDOM.nextInt(i + 1);
+            if (j != i) list.set(i, list.set(j, next));
+        }
+        i++;
+        return next;
+    }
+
+    @Override
+    public void remove() {
+        if (j >= 0 && j < list.size())
+            list.remove(j);
+    }
+}
