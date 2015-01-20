9db8872593c9635ecfa77d13f5a1ca61ac7aaf4c
rwtema <rwtema@gmail.com>
2015-01-20 01:42:29 +0000

    Added a debug method to log which GL11 states are enabled.

--- src/main/java/thermaldynamics/debughelper/DebugHelper.java
+++ src/main/java/thermaldynamics/debughelper/DebugHelper.java
@@ -3,13 +3,13 @@ package thermaldynamics.debughelper;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import java.util.Random;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.Random;
+import org.lwjgl.opengl.GL11;
 
 public class DebugHelper {
     public static boolean debug;
@@ -65,6 +65,93 @@ public class DebugHelper {
     }
 
     public static void quit() {
-        FMLCommonHandler.instance().exitJava(0,true);
+        FMLCommonHandler.instance().exitJava(0, true);
+    }
+
+
+    static String[] glStates = {"GL_ALPHA_TEST",
+            "GL_AUTO_NORMAL",
+            "GL_BLEND",
+            "GL_CLIP_PLANE i",
+            "GL_COLOR_ARRAY",
+            "GL_COLOR_LOGIC_OP",
+            "GL_COLOR_MATERIAL",
+            "GL_CULL_FACE",
+            "GL_DEPTH_TEST",
+            "GL_DITHER",
+            "GL_FOG",
+            "GL_INDEX_ARRAY",
+            "GL_INDEX_LOGIC_OP",
+            "GL_LIGHT i",
+            "GL_LIGHTING",
+            "GL_LINE_SMOOTH",
+            "GL_LINE_STIPPLE",
+            "GL_MAP1_COLOR_4",
+            "GL_MAP1_INDEX",
+            "GL_MAP1_NORMAL",
+            "GL_MAP1_TEXTURE_COORD_1",
+            "GL_MAP1_TEXTURE_COORD_2",
+            "GL_MAP1_TEXTURE_COORD_3",
+            "GL_MAP1_TEXTURE_COORD_4",
+            "GL_MAP1_VERTEX_3",
+            "GL_MAP1_VERTEX_4",
+            "GL_MAP2_COLOR_4",
+            "GL_MAP2_INDEX",
+            "GL_MAP2_NORMAL",
+            "GL_MAP2_TEXTURE_COORD_1",
+            "GL_MAP2_TEXTURE_COORD_2",
+            "GL_MAP2_TEXTURE_COORD_3",
+            "GL_MAP2_TEXTURE_COORD_4",
+            "GL_MAP2_VERTEX_3",
+            "GL_MAP2_VERTEX_4",
+            "GL_NORMAL_ARRAY",
+            "GL_NORMALIZE",
+            "GL_POINT_SMOOTH",
+            "GL_POLYGON_OFFSET_FILL",
+            "GL_POLYGON_OFFSET_LINE",
+            "GL_POLYGON_OFFSET_POINT",
+            "GL_POLYGON_SMOOTH",
+            "GL_POLYGON_STIPPLE",
+            "GL_SCISSOR_TEST",
+            "GL_STENCIL_TEST",
+            "GL_TEXTURE_1D",
+            "GL_TEXTURE_2D",
+            "GL_TEXTURE_COORD_ARRAY",
+            "GL_TEXTURE_GEN_Q",
+            "GL_TEXTURE_GEN_R",
+            "GL_TEXTURE_GEN_S",
+            "GL_TEXTURE_GEN_T",
+            "GL_VERTEX_ARRAY"};
+
+    @SideOnly(Side.CLIENT)
+    public static int[] glCaps;
+
+    @SideOnly(Side.CLIENT)
+    private static void initGLStates() {
+        glCaps = new int[glStates.length];
+
+        for (int i = 0; i < glStates.length; i++) {
+            glCaps[i] = -1;
+            try {
+                glCaps[i] = GL11.class.getField(glStates[i]).getInt(null);
+            } catch (NoSuchFieldException ignored) {
+
+            } catch (IllegalAccessException ignored) {
+
+            }
+        }
+
+    }
+
+    @SideOnly(Side.CLIENT)
+    public static void logGLStates() {
+        if (glCaps == null)
+            initGLStates();
+
+        for (int i = 0; i < glCaps.length; i++) {
+            if (glCaps[i] != -1 && GL11.glIsEnabled(glCaps[i])) {
+                info(glStates[i]);
+            }
+        }
     }
 }
