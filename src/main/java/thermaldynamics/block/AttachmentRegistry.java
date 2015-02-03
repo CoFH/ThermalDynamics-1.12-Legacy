c6c761ee1ac1306fb76100ea0be79e8f8a3544d7
rwtema <rwtema@gmail.com>
2015-02-03 06:56:43 +0000

    Removed dead code.

--- src/main/java/thermaldynamics/block/AttachmentRegistry.java
+++ src/main/java/thermaldynamics/block/AttachmentRegistry.java
@@ -1,7 +1,5 @@
 package thermaldynamics.block;
 
-import java.util.HashMap;
-
 import thermaldynamics.ducts.attachments.facades.Cover;
 import thermaldynamics.ducts.attachments.filter.FilterFluid;
 import thermaldynamics.ducts.attachments.filter.FilterItem;
@@ -9,10 +7,6 @@ import thermaldynamics.ducts.attachments.servo.ServoFluid;
 import thermaldynamics.ducts.attachments.servo.ServoItem;
 
 public class AttachmentRegistry {
-
-	public static HashMap<String, Class<? extends Attachment>> nameToAttachment = new HashMap<String, Class<? extends Attachment>>();
-	public static HashMap<Class<? extends Attachment>, String> attachmentToName = new HashMap<Class<? extends Attachment>, String>();
-
 	public final static byte FACADE = 0;
 	public final static byte SERVO_FLUID = 1;
 	public final static byte SERVO_ITEM = 2;
