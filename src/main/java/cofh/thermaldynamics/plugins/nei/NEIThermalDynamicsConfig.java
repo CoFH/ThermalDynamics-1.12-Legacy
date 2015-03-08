2e3601ce91b8c93ecb43f2ce49ceab3a1a5d09d7
King Lemming <kinglemming@gmail.com>
2015-03-07 21:36:53 -0500

    Refactors in NEI handler.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/thermaldynamics/plugins/nei/NEIThermalDynamicsConfig.java
+++ src/main/java/cofh/thermaldynamics/plugins/nei/NEIThermalDynamicsConfig.java
@@ -6,28 +6,29 @@ import codechicken.nei.api.IConfigureNEI;
 import cofh.thermaldynamics.ThermalDynamics;
 import cofh.thermaldynamics.item.ItemCover;
 
-@SuppressWarnings("UnusedDeclaration")
 public class NEIThermalDynamicsConfig implements IConfigureNEI {
 
-    @Override
-    public void loadConfig() {
+	@Override
+	public void loadConfig() {
 
-        API.registerRecipeHandler(RecipeHandlerCover.instance);
-        API.registerUsageHandler(RecipeHandlerCover.instance);
-        if (!ItemCover.showInNEI)
-            API.hideItem(ItemStackMap.wildcard(ThermalDynamics.itemCover));
-    }
+		API.registerRecipeHandler(RecipeHandlerCover.instance);
+		API.registerUsageHandler(RecipeHandlerCover.instance);
 
-    @Override
-    public String getName() {
+		if (!ItemCover.showInNEI) {
+			API.hideItem(ItemStackMap.wildcard(ThermalDynamics.itemCover));
+		}
+	}
 
-        return ThermalDynamics.modName;
-    }
+	@Override
+	public String getName() {
 
-    @Override
-    public String getVersion() {
+		return ThermalDynamics.modName;
+	}
 
-        return ThermalDynamics.version;
-    }
+	@Override
+	public String getVersion() {
+
+		return ThermalDynamics.version;
+	}
 
 }
