31374be3ed30d1c44c5c72ef3a35cef86b778c82
rwtema <rwtema@gmail.com>
2015-10-25 22:40:36 +0000

    Sneaking while placing a duct prevents connections from forming.

--- src/main/java/cofh/thermaldynamics/block/TileTDBase.java
+++ src/main/java/cofh/thermaldynamics/block/TileTDBase.java
@@ -1053,7 +1053,13 @@ public abstract class TileTDBase extends TileCoFHBase implements IMultiBlock, IT
 	}
 
 	public void onPlacedBy(EntityLivingBase living, ItemStack stack) {
-
+		if (living != null && living.isSneaking() && existsYet()) {
+			for (int i = 0; i < connectionTypes.length; i++) {
+				connectionTypes[i] = ConnectionTypes.BLOCKED;
+			}
+			onNeighborBlockChange();
+			worldObj.notifyBlocksOfNeighborChange(xCoord, yCoord, zCoord, getBlockType());
+		}
 	}
 
 	public void dropAdditional(ArrayList<ItemStack> ret) {
