d0e196285f765c9a110720cde0813f6b06ce0d5e
covers1624 <laughlan.cov@internode.on.net>
2017-02-26 22:58:28 +1030

    Properly implement cover hiding in JEI.

--- src/main/java/cofh/thermaldynamics/plugins/jei/JEIPluginTD.java
+++ src/main/java/cofh/thermaldynamics/plugins/jei/JEIPluginTD.java
@@ -1,10 +1,12 @@
 package cofh.thermaldynamics.plugins.jei;
 
 import cofh.thermaldynamics.duct.attachments.cover.CoverHelper;
+import cofh.thermaldynamics.init.TDItems;
 import cofh.thermaldynamics.init.TDProps;
 import cofh.thermaldynamics.item.ItemCover;
 import mezz.jei.api.BlankModPlugin;
 import mezz.jei.api.IModRegistry;
+import mezz.jei.api.ISubtypeRegistry;
 import mezz.jei.api.JEIPlugin;
 import mezz.jei.api.ingredients.IIngredientBlacklist;
 import net.minecraft.init.Blocks;
@@ -22,10 +24,18 @@ public class JEIPluginTD extends BlankModPlugin {
 		}
 	}
 
+	@Override
+	public void registerItemSubtypes(ISubtypeRegistry subtypeRegistry) {
+		subtypeRegistry.registerSubtypeInterpreter(TDItems.itemCover, (itemStack -> {
+			ItemStack block = CoverHelper.getCoverItemStack(itemStack, false);
+			return "thermaldynamics:cover" + "|" + block.getItem().getRegistryName() + "@" + block.getMetadata();
+		}));
+	}
+
 	private static void blacklistCovers(IIngredientBlacklist ingredientBlacklist) {
 		for (ItemStack stack : ItemCover.getCoverList()) {
 			ItemStack coverBlock = CoverHelper.getCoverItemStack(stack, false);
-			if (coverBlock.getItem() == Item.getItemFromBlock(Blocks.STONE)) {
+			if (coverBlock.getItem() == Item.getItemFromBlock(Blocks.STONE) && coverBlock.getMetadata() == 0) {
 				continue;
 			}
 			ingredientBlacklist.addIngredientToBlacklist(stack);
