74730de62a0f1e9314cef9236270c052201b4f5f
RWTema (other machine) <rwtema@gmail.com>
2017-06-09 23:41:08 +0100

    Transport ducts won't connect through output sides

--- src/main/java/cofh/thermaldynamics/duct/entity/DuctUnitTransport.java
+++ src/main/java/cofh/thermaldynamics/duct/entity/DuctUnitTransport.java
@@ -406,8 +406,7 @@ public class DuctUnitTransport extends DuctUnitTransportBase implements IBlockCo
 
 	@Override
 	public boolean canConnectToOtherDuct(DuctUnit<DuctUnitTransportBase, GridTransport, TransportDestination> adjDuct, byte side, byte oppositeSide) {
-
-		return adjDuct.cast().isRoutable();
+		return parent.getConnectionType(side) != ConnectionType.FORCED && adjDuct.cast().isRoutable();
 	}
 
 	@Nullable
