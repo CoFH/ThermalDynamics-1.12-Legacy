a56430b30edec1856d80b6ea5dd83b9ad78e37a8
covers1624 <laughlan.cov@internode.on.net>
2018-02-26 23:27:34 +1030

    Update ChiselCTM API.

--- src/main/java/cofh/thermaldynamics/block/BlockDuct.java
+++ src/main/java/cofh/thermaldynamics/block/BlockDuct.java
@@ -447,7 +447,7 @@ public class BlockDuct extends BlockTDBase implements IBlockAppearance, IConfigG
 	@Nonnull
 	@Override
 	@Optional.Method (modid = "ctm-api")
-	public IBlockState getFacade(@Nonnull IBlockAccess world, @Nonnull BlockPos pos, @Nullable EnumFacing side) {
+	public IBlockState getFacade(@Nonnull IBlockAccess world, @Nonnull BlockPos pos, @Nullable EnumFacing side, @Nonnull BlockPos connection) {
 
 		return getVisualState(world, pos, side);
 	}
